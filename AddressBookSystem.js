0c40c00ea20c846a937069dfca72bf008b489cc1
MukulDevPatel <patelmukuldev@gmail.com>
2023-07-06 20:14:51 +0530

    [Mukul Dev] Add: We validate contact in address book system using regex method for each one

--- AddressBookSystem.js
+++ AddressBookSystem.js
@@ -9,20 +9,54 @@ class AddressBookSystem{
     PhoneNumber;
     Email;
 
-    constructor (...params){
-        this.FistName = params[0];
-        this.LastName = params[1];
-        this.Address = params[2];
-        this.City = params[3];
-        this.State = params[4];
-        this.Zip = params[5];
-        this.PhoneNumber = params[6];
-        this.Email = params[7];
-    }
-
-    toString () {
-        return "FistName: "+this.FistName+"\nLastName: "+this.LastName+"\nAddress: "+this.Address+"\nCity: "+this.City+
-        "\nState: "+this.State+"\nZip: "+this.Zip+"\nPhone Number: "+this.PhoneNumber+"\nEmail: "+this.Email;
+    constructor(FirstName, LastName, Address, City, State, Zip, PhoneNumber, Email) {
+        // Validate first name
+        if (!/^[A-Z]{1}[a-z]{3,}$/.test(FirstName)) {
+          throw new Error('Invalid first name');
+        }
+        this.FirstName = FirstName;
+    
+        // Validate last name
+        if (!/^[A-Z]{1}[a-z]{3,}$/.test(LastName)) {
+          throw new Error('Invalid last name');
+        }
+        this.LastName = LastName;
+    
+        // Validate Address
+        if (Address.length < 4) {
+          throw new Error('Invalid Address');
+        }
+        this.Address = Address;
+    
+        // Validate City
+        if (City.length < 4) {
+          throw new Error('Invalid City');
+        }
+        this.City = City;
+    
+        // Validate State
+        if (State.length < 4) {
+          throw new Error('Invalid State');
+        }
+        this.State = State;
+    
+        // Validate Zip code
+        if (!/^\d{6}$/.test(Zip)) {
+          throw new Error('Invalid Zip code');
+        }
+        this.Zip = Zip;
+    
+        // Validate phone number
+        if (!/^[0-9]{2}[-][6-9]{1}[0-9]{9}$/.test(PhoneNumber)) {
+          throw new Error('Invalid phone number');
+        }
+        this.PhoneNumber = PhoneNumber;
+    
+        // Validate Email
+        if (!/^[a-zA-Z]+[.+-]{0,1}[a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}$/.test(Email)) {
+          throw new Error('Invalid Email');
+        }
+        this.Email = Email;
     }
 }
 
@@ -32,9 +66,9 @@ let newConatct = new AddressBookSystem(
     "Andheri",
     "Mumbai",
     "Maharshtra",
-    125345,
-    5463254686,
+    "125345",
+    "91-6463254686",
     "patel@gmail.com"
 
 );
-console.log(newConatct.toString());
+console.log(newConatct);
